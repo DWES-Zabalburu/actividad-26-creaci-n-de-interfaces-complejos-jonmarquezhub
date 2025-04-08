2426ef8ca19e7d79675dc58a2fdbf6b52b469a92
jonmarquezhub <jon.marquez.vazquez@zabalburu.org>
2025-04-08 13:55:28 +0200

    Primer Formulario terminado (falta BOLD en botones)

---
+++ Actividad26/src/main/java/org/zabalburu/daw1/actividad26/Actividad26.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1.actividad26;
+
+import org.zabalburu.daw1.actividad26.modelo.Formulario;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Actividad26 {
+
+    public static void main(String[] args) {
+        new Formulario().setVisible(true);
+    }
+}
